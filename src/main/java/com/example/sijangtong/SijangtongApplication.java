8b3db64d26188ad9088eb3915f8173e6f4935ead
brian110326 <brian110326@gmail.com>
2024-05-08 15:17:15 +0900

    sijangtong 프로젝트 생성

---
+++ src/main/java/com/example/sijangtong/SijangtongApplication.java
@@ -0,0 +1,13 @@
+package com.example.sijangtong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SijangtongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SijangtongApplication.class, args);
+	}
+
+}
