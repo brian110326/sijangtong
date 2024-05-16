46042391e2c383613ba103e2ce6e641fbd77efbe
brian110326 <brian110326@gmail.com>
2024-05-16 15:25:33 +0900

    review service 수정

--- src/main/java/com/example/sijangtong/dto/ProductDto.java
+++ src/main/java/com/example/sijangtong/dto/ProductDto.java
@@ -30,6 +30,8 @@ public class ProductDto {
 
     private Long orderId;
 
+    private double avg;
+
     // 1개의 product에 여러장의 productimg
     // product를 기준으로 productimg를 찾을 수도 있으니
     @Builder.Default
