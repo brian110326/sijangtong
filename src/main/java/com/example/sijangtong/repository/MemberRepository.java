a102719bb4f0d28ca0459418b2988f9aa1ef4caa
brian110326 <brian110326@gmail.com>
2024-05-19 20:26:10 +0900

    member repository 작성

--- src/main/java/com/example/sijangtong/repository/MemberRepository.java
+++ src/main/java/com/example/sijangtong/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.example.sijangtong.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.sijangtong.entity.Member;
 import com.example.sijangtong.repository.memberTotal.MemberReviewRepository;
@@ -10,4 +12,12 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, String>, MemberReviewRepository {
 
     Optional<Member> findByMemberEmail(String memberEmail);
+
+    @Modifying
+    @Query("update Member m set m.memberNickname = :memberNickname where m.memberEmail = :memberEmail")
+    void updateNickname(String memberNickname, String memberEmail);
+
+    @Modifying
+    @Query("update Member m set m.memberAddress = :memberAddress where m.memberEmail = :memberEmail")
+    void updateAddress(String memberAddress, String memberEmail);
 }
