00f7f61bee1aa771502de403e2c4fb1f8740b2ee
brian110326 <brian110326@gmail.com>
2024-05-19 19:12:37 +0900

    MemberRepository 작성

--- src/main/java/com/example/sijangtong/repository/MemberRepository.java
+++ src/main/java/com/example/sijangtong/repository/MemberRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.sijangtong.entity.Member;
 import com.example.sijangtong.repository.memberTotal.MemberReviewRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String>, MemberReviewRepository {
 
+    Optional<Member> findByMemberEmail(String memberEmail);
 }
