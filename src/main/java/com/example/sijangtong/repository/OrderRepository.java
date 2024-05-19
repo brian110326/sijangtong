e1de95954bc93de7defe89fc4f3d456b3b5cc9ef
brian110326 <brian110326@gmail.com>
2024-05-19 20:26:20 +0900

    order repository 작성

--- src/main/java/com/example/sijangtong/repository/OrderRepository.java
+++ src/main/java/com/example/sijangtong/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.example.sijangtong.constant.OrderPayment;
+import com.example.sijangtong.entity.Member;
 import com.example.sijangtong.entity.Order;
 import com.example.sijangtong.entity.OrderItem;
 import com.example.sijangtong.entity.Product;
@@ -21,6 +22,12 @@ public interface OrderRepository extends JpaRepository<Order, Long>, OrderOrderI
     @Modifying
     @Query("delete from Order o where o.store = :store")
     void deleteByStore(Store store);
+
+    @Modifying
+    @Query("delete from Order o where o.member = :member")
+    void deleteByMember(Member member);
+
+    List<Order> findByMember(Member member);
 
     @Modifying
     @Query("update Order o set o.orderPayment = :orderPayment where o.orderId = :orderId")
