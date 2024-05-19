59a27d663e6140e6b2a6bb4b37092f9d148ded85
brian110326 <brian110326@gmail.com>
2024-05-19 19:12:23 +0900

    AuthMemberDto 작성

---
+++ src/main/java/com/example/sijangtong/dto/AuthMemberDto.java
@@ -0,0 +1,26 @@
+package com.example.sijangtong.dto;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class AuthMemberDto extends User {
+
+    private MemberDto memberDto;
+
+    public AuthMemberDto(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+
+    }
+
+    public AuthMemberDto(MemberDto memberDto) {
+        super(memberDto.getMemberEmail(), memberDto.getMemberPwd(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + memberDto.getMemberRole())));
+
+        this.memberDto = memberDto;
+    }
+
+}
