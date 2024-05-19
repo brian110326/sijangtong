c583a296f93a9cc965b0d531cad7322646d8c98e
brian110326 <brian110326@gmail.com>
2024-05-19 20:29:37 +0900

    pom.xml security 임시 주석처리

--- src/main/java/com/example/sijangtong/config/SecurityConfig.java
+++ src/main/java/com/example/sijangtong/config/SecurityConfig.java
@@ -1,46 +1,53 @@
-package com.example.sijangtong.config;
+// package com.example.sijangtong.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import
+// org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+// import
+// org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import
+// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.http.SessionCreationPolicy;
+// import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// @EnableMethodSecurity
+// public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+// @Bean
+// SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(
-                authorize -> authorize
-                        // 로그인 전 허용 화면 및 static 폴더
-                        .requestMatchers("/", "/assets/**", "/css/**", "/image/**", "/js/**", "/lib/**").permitAll()
-                        .requestMatchers("/shop/list", "/shop/read", "/shop/home", "/shop/storeDetail").permitAll()
-                        // UploadController : 이미지 보여주기
-                        .requestMatchers("/upload/display").permitAll()
-                        .requestMatchers("/member/register").permitAll());
+// http.authorizeHttpRequests(
+// authorize -> authorize
+// // 로그인 전 허용 화면 및 static 폴더
+// .requestMatchers("/", "/assets/**", "/css/**", "/image/**", "/js/**",
+// "/lib/**").permitAll()
+// .requestMatchers("/shop/list", "/shop/read", "/shop/home",
+// "/shop/storeDetail").permitAll()
+// // UploadController : 이미지 보여주기
+// .requestMatchers("/upload/display").permitAll()
+// .requestMatchers("/member/register").permitAll());
 
-        http.formLogin(login -> login.loginPage("/member/login").permitAll()
-                .defaultSuccessUrl("/shop/list", true));
+// http.formLogin(login -> login.loginPage("/member/login").permitAll()
+// .defaultSuccessUrl("/shop/list", true));
 
-        http.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                .logoutSuccessUrl("/"));
+// http.logout(logout -> logout.logoutRequestMatcher(new
+// AntPathRequestMatcher("/member/logout"))
+// .logoutSuccessUrl("/"));
 
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+// http.sessionManagement(session ->
+// session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 
-        return http.build();
-    }
+// return http.build();
+// }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-}
+// @Bean
+// PasswordEncoder passwordEncoder() {
+// return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+// }
+// }
--- src/main/java/com/example/sijangtong/dto/AuthMemberDto.java
+++ src/main/java/com/example/sijangtong/dto/AuthMemberDto.java
@@ -1,26 +1,27 @@
-package com.example.sijangtong.dto;
+// package com.example.sijangtong.dto;
 
-import java.util.Collection;
-import java.util.List;
+// import java.util.Collection;
+// import java.util.List;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import org.springframework.security.core.userdetails.User;
 
-public class AuthMemberDto extends User {
+// public class AuthMemberDto extends User {
 
-    private MemberDto memberDto;
+// private MemberDto memberDto;
 
-    public AuthMemberDto(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, authorities);
+// public AuthMemberDto(String username, String password, Collection<? extends
+// GrantedAuthority> authorities) {
+// super(username, password, authorities);
 
-    }
+// }
 
-    public AuthMemberDto(MemberDto memberDto) {
-        super(memberDto.getMemberEmail(), memberDto.getMemberPwd(),
-                List.of(new SimpleGrantedAuthority("ROLE_" + memberDto.getMemberRole())));
+// public AuthMemberDto(MemberDto memberDto) {
+// super(memberDto.getMemberEmail(), memberDto.getMemberPwd(),
+// List.of(new SimpleGrantedAuthority("ROLE_" + memberDto.getMemberRole())));
 
-        this.memberDto = memberDto;
-    }
+// this.memberDto = memberDto;
+// }
 
-}
+// }
