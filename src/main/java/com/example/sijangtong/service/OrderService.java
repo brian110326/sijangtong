662023ea3198dc301838211c5e6b06bb5c3a47d6
brian110326 <brian110326@gmail.com>
2024-05-23 22:38:57 +0900

    order 수정

--- src/main/java/com/example/sijangtong/service/OrderService.java
+++ src/main/java/com/example/sijangtong/service/OrderService.java
@@ -18,11 +18,10 @@ import java.util.stream.Collectors;
 public interface OrderService {
   // 주문 리스트 보여주기
   PageResultDto<OrderDto, Object[]> getOrderList(
-    PageRequestDto pageRequestDto,
-    Long storeId
-  );
+      PageRequestDto pageRequestDto,
+      Long storeId);
 
-  // 주문 취소(고객이)
+  // 주문 취소
   Long removeOrder(Long orderId);
 
   // 주문 자체 취소가 아닌 주문 안에 주문아이템만 삭제
@@ -37,80 +36,59 @@ public interface OrderService {
   // 주문하기
   Long createOrder(OrderDto orderDto);
 
-  public default OrderDto entityToDto(
-    Order order,
-    List<OrderItem> orderItems,
-    Rider rider,
-    Member member,
-    List<Product> products,
-    Store store
-  ) {
+  public default OrderDto entityToDto(Order order, List<OrderItem> orderItems) {
     OrderDto orderDto = OrderDto
-      .builder()
-      .orderId(order.getOrderId())
-      .orderAddress(order.getOrderAddress())
-      .riderOrdercancel(order.getRiderOrdercancel())
-      .orderPayment(order.getOrderPayment())
-      .memberEmail(order.getMember().getMemberEmail())
-      .storeId(order.getStore().getStoreId())
-      .rider(order.getRider())
-      .createdDate(order.getCreatedDate())
-      .lastModifiedDate(order.getLastModifiedDate())
-      .build();
+        .builder()
+        .orderId(order.getOrderId())
+        .orderAddress(order.getOrderAddress())
+        .riderOrdercancel(order.getRiderOrdercancel())
+        .orderPayment(order.getOrderPayment())
+        .memberEmail(order.getMember().getMemberEmail())
+        .storeId(order.getStore().getStoreId())
+        .rider(order.getRider())
+        .createdDate(order.getCreatedDate())
+        .lastModifiedDate(order.getLastModifiedDate())
+        .build();
 
-    List<ProductDto> productDtos = products
-      .stream()
-      .map(product -> {
-        return ProductDto
-          .builder()
-          .productId(product.getProductId())
-          .pName(product.getPName())
-          .price(product.getPrice())
-          .amount(product.getAmount())
-          .storeId(product.getStore().getStoreId())
-          .build();
-      })
-      .collect(Collectors.toList());
+    List<OrderItemDto> orderItemDtos = orderItems
+        .stream()
+        .map(orderItem -> {
+          Product product = orderItem.getProduct();
 
-    // List<OrderItemDto> orderItemDtos = orderItems
-    //     .stream()
-    //     .map(orderItem -> {
-    //       return OrderItemDto
-    //           .builder()
-    //           .id(orderItem.getId())
-    //           .productDtos(productDtos)
-    //           .orderId(orderItem.getOrder().getOrderId())
-    //           // orderPrice는 주문수량 * product의 원가격
-    //           .orderPrice(orderItem.getOrderPrice())
-    //           .orderAmount(orderItem.getOrderAmount())
-    //           .build();
-    //     })
-    //     .collect(Collectors.toList());
+          return OrderItemDto
+              .builder()
+              .id(orderItem.getId())
+              .orderId(orderItem.getOrder().getOrderId())
+              // orderPrice는 주문수량 * product의 원가격
+              .orderPrice(orderItem.getOrderPrice())
+              .orderAmount(orderItem.getOrderAmount())
+              .productId(product.getProductId())
+              .build();
+        })
+        .collect(Collectors.toList());
 
-    // orderDto.setOrderItemDtos(orderItemDtos);
-
-    // 하나의 주문에 여러개의 orderItem, orderItem 안에 여러개의 product존재
+    orderDto.setOrderItemDtos(orderItemDtos);
 
     return orderDto;
   }
 
   public default Order dtoToEntity(OrderDto orderDto) {
     Member member = Member
-      .builder()
-      .memberEmail(orderDto.getMemberEmail())
-      .build();
+        .builder()
+        .memberEmail(orderDto.getMemberEmail())
+        .build();
     Store store = Store.builder().storeId(orderDto.getStoreId()).build();
 
     Order order = Order
-      .builder()
-      .orderId(orderDto.getOrderId())
-      .orderAddress(orderDto.getOrderAddress())
-      .orderPayment(orderDto.getOrderPayment())
-      .member(member)
-      .store(store)
-      .rider(orderDto.getRider())
-      .riderOrdercancel(orderDto.getRiderOrdercancel())
-      .build();
+        .builder()
+        .orderId(orderDto.getOrderId())
+        .orderAddress(orderDto.getOrderAddress())
+        .orderPayment(orderDto.getOrderPayment())
+        .member(member)
+        .store(store)
+        .rider(orderDto.getRider())
+        .riderOrdercancel(orderDto.getRiderOrdercancel())
+        .build();
 
     order.setCreatedDate(orderDto.getCreatedDate());
     order.setLastModifiedDate(orderDto.getLastModifiedDate());
