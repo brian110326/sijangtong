3b72182bc26b514ce65c4b4e86e4ea99e42af72b
brian110326 <brian110326@gmail.com>
2024-05-30 21:29:37 +0900

    .

--- target/generated-sources/annotations/com/example/sijangtong/entity/QMember.java
+++ target/generated-sources/annotations/com/example/sijangtong/entity/QMember.java
@@ -2,11 +2,13 @@ package com.example.sijangtong.entity;
 
 import static com.querydsl.core.types.PathMetadataFactory.*;
 
-import com.querydsl.core.types.Path;
-import com.querydsl.core.types.PathMetadata;
 import com.querydsl.core.types.dsl.*;
-import com.querydsl.core.types.dsl.PathInits;
+
+import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
 
 /**
  * QMember is a Querydsl query type for Member
@@ -14,56 +16,44 @@ import javax.annotation.processing.Generated;
 @Generated("com.querydsl.codegen.DefaultEntitySerializer")
 public class QMember extends EntityPathBase<Member> {
 
-  private static final long serialVersionUID = 321388034L;
+    private static final long serialVersionUID = 321388034L;
 
-  private static final PathInits INITS = PathInits.DIRECT2;
+    private static final PathInits INITS = PathInits.DIRECT2;
 
-  public static final QMember member = new QMember("member1");
+    public static final QMember member = new QMember("member1");
 
-  public final StringPath memberAddress = createString("memberAddress");
+    public final StringPath memberAddress = createString("memberAddress");
 
-  public final StringPath memberEmail = createString("memberEmail");
+    public final StringPath memberEmail = createString("memberEmail");
 
-  public final StringPath memberNickname = createString("memberNickname");
+    public final StringPath memberNickname = createString("memberNickname");
 
-  public final StringPath memberPwd = createString("memberPwd");
+    public final StringPath memberPwd = createString("memberPwd");
 
-  public final EnumPath<com.example.sijangtong.constant.MemberRole> memberRole = createEnum(
-    "memberRole",
-    com.example.sijangtong.constant.MemberRole.class
-  );
+    public final EnumPath<com.example.sijangtong.constant.MemberRole> memberRole = createEnum("memberRole", com.example.sijangtong.constant.MemberRole.class);
 
-  public final QStore store;
+    public final QStore store;
 
-  public QMember(String variable) {
-    this(Member.class, forVariable(variable), INITS);
-  }
+    public QMember(String variable) {
+        this(Member.class, forVariable(variable), INITS);
+    }
 
-  public QMember(Path<? extends Member> path) {
-    this(
-      path.getType(),
-      path.getMetadata(),
-      PathInits.getFor(path.getMetadata(), INITS)
-    );
-  }
+    public QMember(Path<? extends Member> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
 
-  public QMember(PathMetadata metadata) {
-    this(metadata, PathInits.getFor(metadata, INITS));
-  }
+    public QMember(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
 
-  public QMember(PathMetadata metadata, PathInits inits) {
-    this(Member.class, metadata, inits);
-  }
+    public QMember(PathMetadata metadata, PathInits inits) {
+        this(Member.class, metadata, inits);
+    }
 
-  public QMember(
-    Class<? extends Member> type,
-    PathMetadata metadata,
-    PathInits inits
-  ) {
-    super(type, metadata, inits);
-    this.store =
-      inits.isInitialized("store")
-        ? new QStore(forProperty("store"), inits.get("store"))
-        : null;
-  }
+    public QMember(Class<? extends Member> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.store = inits.isInitialized("store") ? new QStore(forProperty("store"), inits.get("store")) : null;
+    }
+
 }
+
